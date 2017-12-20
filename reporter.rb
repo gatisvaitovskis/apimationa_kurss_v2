eaed2f824a2411fbde61fd95594b06e7bcd011fa
Gatis Vaitovskis <Gatis.Vaitovskis@parts-advisor.com>
2017-12-20 18:34:10 +0200

    m

--- reporter.rb
+++ reporter.rb
@@ -1,14 +1,18 @@
 require 'json'
 require_relative 'features/support/api_helper.rb'
 
+build_number = ARGV[0]
+job_url = ARGV[1]
+
+
 thumbnail = { 'url' => 'https://i.ytimg.com/vi/qh7LLydY8eo/maxresdefault.jpg' }
 fields = []
 
-fields.push({ 'name' => 'Gaitors', 'value' => 'GAJAKA' })
-fields.push({ 'name' => 'AUTOMATIONS', 'value' => 'Bisk nofieloja' })
+fields.push({ 'name' => 'Jenkins job', 'value' => job_url.to_s })
+fields.push({ 'name' => 'Bildes numberas', 'value' => build_number.to_s })
 
 embed = []
-embed.push({ 'Title' => 'QA TEST',
+embed.push({
              'color' => 16711909,
              'fields' => fields,
              'thumbnail' => thumbnail })
